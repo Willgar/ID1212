5f8f1a988dcca0989d25ab09e6826900b9878d88
William <Williamaxb@gmail.com>
2021-11-05 17:32:06 +0100

    202111051732

--- Task2/src/HTTP.java
+++ Task2/src/HTTP.java
@@ -1,6 +1,8 @@
 import java.io.*;
+import java.net.HttpURLConnection;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.net.URL;
 import java.util.Random;
 
 import static java.lang.System.in;
@@ -16,15 +18,12 @@ public class HTTP {
                 Socket socket = serverSocket.accept();
                 int[] cookieID = checkRequest(socket);
                 System.out.println("Connection Receieved with socket: " + socket);
-                if(servers[cookieID[0]] != null) {
-                    //servers[cookieID].guess(cookieID[1]);
-                    System.out.println("it worked"+ cookieID);
+                if(servers[cookieID[0]] != null && cookieID[0] != 99) {
+                    servers[cookieID[0]].guess(cookieID[1]);
                 } else {
-                    System.out.println("test");
                     HTTPServer newServer = new HTTPServer(socket, clientID);
                     servers[clientID] = newServer;
-                    //servers[clientID].start();
-                    newServer.start();
+                    servers[clientID].start();
                     clientID++;
                 }
             }
@@ -34,31 +33,34 @@ public class HTTP {
     }
 
     static int[] checkRequest(Socket socket) throws IOException {
-        int[] returnArray = new int[2];
-        returnArray[0] = -1;
-        returnArray[1] = -1;
+        int[] returnArray = {99,-1};
+        boolean flag = true;
+        boolean flag2 = false;
         BufferedReader buffer = new BufferedReader(new InputStreamReader(socket.getInputStream()));
         String msg ="";
         while ((msg = buffer.readLine())!= null) {
+            if(msg.contains("Accept-Language")){//nice hardcode
+                msg = buffer.readLine();
+                flag2 = true;
+            }
             if(msg.contains("Cookie")){
                 msg = msg.split("=")[1];
                 msg = msg.split(" ")[0];
-                System.out.println(msg);
+                System.out.println("COOKIE: "+ msg);
                 returnArray[0] = Integer.parseInt(msg);
             }
-            if(msg.contains("guess")){
+            if(msg.contains("guess")&&flag){
                 msg = msg.split("=")[1];
                 msg = msg.split(" ")[0];
-                System.out.println(msg);
+                System.out.println("GUESS: "+ msg);
+                flag=false;
                 returnArray[1] = Integer.parseInt(msg);
             }
-            if(returnArray[0] != -1 && returnArray[1] != -1){
+            if((returnArray[0] != 99 && returnArray[1] != -1) || flag2){
                 return returnArray;
             }
         }
-        System.out.println("no cookie");
-        int[] arr = {-1,-1};
-        return arr;
+        return returnArray;
     }
     static int getGuess(Socket socket) throws IOException {
         BufferedReader buffer = new BufferedReader(new InputStreamReader(socket.getInputStream()));
@@ -80,151 +82,80 @@ class HTTPServer extends Thread {
     Socket socket;
     int clientID;
     int correctValue;
-    Random rand = new Random();
-    String guessValue;
     int guesses;
+    PrintWriter output;
 
-    public HTTPServer(Socket socket, int clientID){
+    public HTTPServer(Socket socket, int clientID) throws IOException {
         this.socket = socket;
         this.clientID = clientID;
         this.guesses = 0;
+        Random rand = new Random();
         this.correctValue = rand.nextInt(100);
+        this.output = new PrintWriter(socket.getOutputStream());
     }
 
     public void run() {
-        PrintWriter output = null;
-        try {
-            System.out.println("hello");
-            //BufferedReader buffer = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-            output = new PrintWriter(socket.getOutputStream());
-            output.println("HTTP/1.1 200 OK");
-            output.println("Set-Cookie: clientId=" + clientID + " expires=Wednesday,24-Dec-21 23:59:59 GMT");
-            output.println("Content-Type: text/html");
-            output.println("\r\n");
-            output.println("<p> Welcome to the guessing game </p>");
-            output.println("<p> guess between 1 and 100 </p>");
-            output.println("<p> Guess </p>");
-            //output.println("<form action=\"\" method=\"get\"> <label for=\"guess\"> guess number <input type=\"text\" id=\"guess\" name=\"guess\"> </label></label>  </form> ");
-            output.println();
-            output.flush();
-            System.out.println("hello2");
-
-            //if((msg = buffer.readLine()).contains("guess")){
-                /*if(Integer.parseInt(guessValue) == correctValue){
-                    //guess higher
-                    output.println("HTTP/1.1 200 OK");
-                    output.println("Content-Type: text/html");
-                    output.println("\r\n");
-                    output.println("<p> You won. You have made " + guesses + " guesses </p>");
-                    output.println("<p> Guess </p>");
-                    output.println("<form action=\"\" method=\"get\"> <label for=\"guess\"> guess number <input type=\"text\" id=\"guess\" name=\"guess\"> </label></label>  </form> ");
-                    output.println();
-                    output.flush();
-                } else if(Integer.parseInt(guessValue) < correctValue) {
-                    //guess higher
-                    output.println("HTTP/1.1 200 OK");
-                    output.println("Content-Type: text/html");
-                    output.println("\r\n");
-                    output.println("<p> Nope, guess higher. You have made " + ++guesses + " guesses so far </p>");
-                    output.println("<p> Guess </p>");
-                    output.println("<form action=\"\" method=\"get\"> <label for=\"guess\"> guess number <input type=\"text\" id=\"guess\" name=\"guess\"> </label></label>  </form> ");
-                    output.println();
-                    output.flush();
-                } else {
-                    //guess lower
-                    guesses++;
-                    output.println("HTTP/1.1 200 OK");
-                    output.println("Content-Type: text/html");
-                    output.println("\r\n");
-                    output.println("<p> Nope, guess lower. You have made " + ++guesses + " guesses so far </p>");
-                    output.println("<p> Guess </p>");
-                    output.println("<form action=\"\" method=\"get\"> <label for=\"guess\"> guess number <input type=\"text\" id=\"guess\" name=\"guess\"> </label></label>  </form> ");
-                    output.println();
-                    output.flush();
-                }
-           // }
-
-*/
-
-
-            //new HTTPGuess(socket, clientID).start();
-
-        } catch (IOException e) {
-            e.printStackTrace();
+        sendStartPage();
+    }
+    public void guess(int guess){
+        if(guess==correctValue){
+            sendWinPage();
+        } else if(guess < correctValue){
+            sendHigherPage();
+        } else{
+            sendLowerPage();
         }
     }
-}
-
-class HTTPGuess extends Thread {
-    Socket socket;
-    int guesses;
-    Random rand = new Random();
-    int correctValue;
-    String guessValue;
-    int clientID;
-
-    public HTTPGuess(Socket socket, int clientID){
-        this.socket = socket;
-        this.guesses = 0;
-        this.correctValue = rand.nextInt(100);
-        this.clientID = clientID;
+    private void sendStartPage(){
+        output.println("HTTP/1.1 200 OK");
+        output.println("Set-Cookie: clientId=" + clientID + " expires=Wednesday,24-Dec-21 23:59:59 GMT");
+        output.println("Content-Type: text/html");
+        output.println("Connection: closed");
+        output.println("\r\n");
+        output.println("<p> Welcome to the guessing game </p>");
+        output.println("<p> guess between 1 and 100 </p>");
+        output.println("<p> Guess </p>");
+        output.println("<form action=\"\" method=\"get\"> <label for=\"guess\"> guess number <input onClick=\" window.location.href = \"http://localhost:8282/?guess=\"document.getElementById(\"guess\")\" type=\"text\" id=\"guess\" name=\"guess\"> </label></label>  </form> ");
+        output.println();
+        output.flush();
+        System.out.println("Game Started");
     }
-    public void run() {
-        try {
-            BufferedReader buffer = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-            PrintWriter output = new PrintWriter(socket.getOutputStream());
-            String msg="";
-            int q = 0;
-            while ((msg = buffer.readLine())!= null && q < 5) {
-                if(msg.contains("guess")){
-                    guessValue = msg.split("=")[1];
-                    guessValue = guessValue.split(" ")[0];
-                    System.out.println(guessValue);
-                    break;
-                }
-                q++;
-            }
-            System.out.println(guessValue);
-            if(Integer.parseInt(guessValue) == correctValue){
-                //guess higher
-                output.println("HTTP/1.1 200 OK");
-                output.println("Set-Cookie: clientId=" + clientID + " expires=Wednesday,24-Dec-21 23:59:59 GMT");
-                output.println("Content-Type: text/html");
-                output.println("\r\n");
-                output.println("<p> You won. You have made " + guesses + " guesses </p>");
-                output.println("<p> Guess </p>");
-                output.println("<form action=\"\" method=\"get\"> <label for=\"guess\"> guess number <input type=\"text\" id=\"guess\" name=\"guess\"> </label></label>  </form> ");
-                output.println();
-                output.flush();
-            } else if(Integer.parseInt(guessValue) < correctValue) {
-                //guess higher
-                output.println("HTTP/1.1 200 OK");
-                output.println("Set-Cookie: clientId=" + rand.nextInt(100) + " expires=Wednesday,24-Dec-21 23:59:59 GMT");
-                output.println("Content-Type: text/html");
-                output.println("\r\n");
-                output.println("<p> Nope, guess higher. You have made " + ++guesses + " guesses so far </p>");
-                output.println("<p> Guess </p>");
-                output.println("<form action=\"\" method=\"get\"> <label for=\"guess\"> guess number <input type=\"text\" id=\"guess\" name=\"guess\"> </label></label>  </form> ");
-                output.println();
-                output.flush();
-            } else {
-                //guess lower
-                guesses++;
-                output.println("HTTP/1.1 200 OK");
-                output.println("Set-Cookie: clientId=" + rand.nextInt(100) + " expires=Wednesday,24-Dec-21 23:59:59 GMT");
-                output.println("Content-Type: text/html");
-                output.println("\r\n");
-                output.println("<p> Nope, guess lower. You have made " + ++guesses + " guesses so far </p>");
-                output.println("<p> Guess </p>");
-                output.println("<form action=\"\" method=\"get\"> <label for=\"guess\"> guess number <input type=\"text\" id=\"guess\" name=\"guess\"> </label></label>  </form> ");
-                output.println();
-                output.flush();
-            }
+    private void sendWinPage(){
+        output.println("HTTP/1.1 200 OK");
+        output.println("Content-Type: text/html");
+        output.println("\r\n");
+        output.println("<p> You won. You have made " + guesses + " guesses </p>");
+        output.println("<p> Guess </p>");
+        output.println("<form action=\"\" method=\"get\"> <label for=\"guess\"> guess number <input type=\"text\" id=\"guess\" name=\"guess\"> </label></label>  </form> ");
+        output.println();
+        output.flush();
+        System.out.println("Game Won");
 
+    }
+    private void sendHigherPage(){
+        output.println("HTTP/1.1 200 OK");
+        output.println("Connection: closed");
+        output.println("Content-Type: text/html");
+        output.println("\r\n");
+        output.println("<p> Nope, guess higher. You have made " + ++guesses + " guesses so far </p>");
+        output.println("<p> Guess </p>");
+        output.println("<form action=\"\" method=\"get\"> <label for=\"guess\"> guess number <input type=\"text\" id=\"guess\" name=\"guess\"> </label></label>  </form> ");
+        output.println();
+        output.flush();
+        System.out.println("Guess Higher");
 
+    }
+    private void sendLowerPage(){
+        output.println("HTTP/1.1 200 OK");
+        output.println("Connection: closed");
+        output.println("Content-Type: text/html");
+        output.println("\r\n");
+        output.println("<p> Nope, guess lower. You have made " + ++guesses + " guesses so far </p>");
+        output.println("<p> Guess </p>");
+        output.println("<form action=\"\" method=\"get\"> <label for=\"guess\"> guess number <input type=\"text\" id=\"guess\" name=\"guess\"> </label></label>  </form> ");
+        output.println();
+        output.flush();
+        System.out.println("Guess Lower");
 
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
     }
 }
