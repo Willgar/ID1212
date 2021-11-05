dedc88a5741638e7b88b0b92c88e4fcb22561fd3
William <williamaxb@gmail.com>
2021-11-05 09:58:37 +0100

    lab 2 WIP

--- Task2/src/HTTP.java
+++ Task2/src/HTTP.java
@@ -1,10 +1,12 @@
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.Random;
 
 public class HTTP {
     public static void main(String[] args) throws IOException {
-        ServerSocket serverSocket = new ServerSocket(8080);
+        ServerSocket serverSocket = new ServerSocket(8282);
 
         try {
             while (true) {
@@ -26,6 +28,22 @@ class HTTPServer extends Thread {
     }
 
     public void run() {
+        PrintWriter output = null;
+        Random rand = new Random();
+        try {
+            output = new PrintWriter(socket.getOutputStream());
+            System.out.println("hello");
+            output.println("HTTP/1.1 200 OK");
+            output.println("Content-Type: text/html");
+            output.println("Set-Cookie: clientId=" + rand.nextInt(100) + " expires=Wednesday,24-Dec-21 23:59:59 GMT");
+            output.println();
+            output.flush();
+            System.out.println("hello2");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
 
     }
 }
