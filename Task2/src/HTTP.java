ece8c6f764d03499f0796a2db349f212ae036321
William <Williamaxb@gmail.com>
2021-11-09 11:38:35 +0100

    Task 2 and Extra Assignment

--- Task2/src/HTTP.java
+++ Task2/src/HTTP.java
@@ -4,24 +4,55 @@ import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.util.Random;
+import java.util.concurrent.TimeUnit;
+
 class Client {
     public static void main(String[] args) throws IOException, InterruptedException {
         HttpClient client = HttpClient.newHttpClient();
-        HttpRequest request = HttpRequest.newBuilder()
-                .uri(URI.create("http://localhost:8282/?guess=25"))
-                .header("clientID", "1")
-                .build();
 
-        HttpResponse<String> response =
-                client.send(request, HttpResponse.BodyHandlers.ofString());
+        double tries = 0;
+        for(int i = 0; i <100; i++) {
+            int guesses = 50;
+            int previous = 100;
+            int j = 0;
+            boolean flag = true;
+            while (flag) {
+                HttpRequest request = HttpRequest.newBuilder()
+                        .uri(URI.create("http://localhost:8282/?guess=" + guesses))
+                        .header("Cookie", "clientId="+i+" expires=Wednesday,24-Dec-21 23:59:59 GMT")
+                        .build();
+                HttpResponse<String> response =
+                        client.send(request, HttpResponse.BodyHandlers.ofString());
+                //checks the game logic and changes the guess accordingly
+                if(response.body().contains("won")) {
+                    System.out.println("It took " + j + " guesses before you won");
+                    tries = tries+j;
+                    flag=false;
+                    break;
+                } else if(response.body().contains("higher")){
+                    int temp = guesses;
+                    guesses += (Math.abs(guesses-previous)/2);
+                    previous = temp;
+                    if(guesses==temp)
+                        guesses++;
+                }else if(response.body().contains("lower")){
+                    int temp = guesses;
+                    guesses -= (Math.abs(guesses-previous)/2);
+                    previous = temp;
+                    if(guesses==temp)
+                        guesses--;
+                }
+                j++;
+            }
 
-        System.out.println(response.body());
+        }
+        System.out.println("Average: " +tries/100 + " guesses per match");
     }
 }
 public class HTTP {
     public static void main(String[] args) throws IOException, URISyntaxException, InterruptedException {
         ServerSocket serverSocket = new ServerSocket(8282);
-        HTTPServer[] servers = new HTTPServer[100];
+        HTTPServer[] servers = new HTTPServer[1000];
         int clientID = 0;
         try {
             while (true) {
@@ -29,7 +60,7 @@ public class HTTP {
                 Socket socket = serverSocket.accept();
                 int[] cookieID = checkRequest(socket);
                 System.out.println("Connection Receieved with socket: " + socket);
-                if(servers[cookieID[0]] != null && cookieID[0] != 99 && cookieID[1] != -1) {
+                if(servers[cookieID[0]] != null && cookieID[1] != -1) {
                     servers[cookieID[0]].guess(cookieID[1], socket);
                 } else {
                     HTTPServer newServer = new HTTPServer(socket, clientID);
@@ -43,14 +74,13 @@ public class HTTP {
         }
     }
 
-    static int[] checkRequest(Socket socket) throws IOException {
-        int[] returnArray = {99,-1};
+    static int[] checkRequest(Socket socket) throws IOException {  //Custom parsing of the HTTP Request receieved
+        int[] returnArray = {999,-1};                              // to find Cookies and guess queries
         boolean flag = true;
         boolean flag2 = false;
         BufferedReader buffer = new BufferedReader(new InputStreamReader(socket.getInputStream()));
         String msg ="";
         while ((msg = buffer.readLine())!= null) {
-            System.out.println(msg);
             if(msg.contains("Accept-Language")){//nice hardcode
                 msg = buffer.readLine();
                 flag2 = true;
@@ -68,7 +98,7 @@ public class HTTP {
                 flag=false;
                 returnArray[1] = Integer.parseInt(msg);
             }
-            if((returnArray[0] != 99 && returnArray[1] != -1) || flag2){
+            if((returnArray[0] != 999 && returnArray[1] != -1) || flag2){
                 return returnArray;
             }
         }
@@ -82,12 +112,13 @@ class HTTPServer extends Thread {
     int correctValue;
     int guesses;
     private int[] doublettes;
+    Random rand;
 
     public HTTPServer(Socket socket, int clientID) throws IOException {
         this.socket = socket;
         this.clientID = clientID;
         this.guesses = 0;
-        Random rand = new Random();
+        rand = new Random();
         this.correctValue = rand.nextInt(100);
         doublettes = new int[100];
     }
@@ -100,19 +131,19 @@ class HTTPServer extends Thread {
         }
     }
     public void guess(int guess, Socket socket) throws IOException {
-        if(doublettes[guess]!=1){
+        if(doublettes[guess]!=1){               //To avoid double guessing errors the guess only counts once per number
             doublettes[guess]++;
             guesses++;
+        }
             System.out.println("Guessed: " + guess + "\nCorrect: " + correctValue + "\nGuesses: " + guesses);
-            PrintWriter output = new PrintWriter(socket.getOutputStream(), true);
-            if(guess==correctValue){
+            PrintWriter output = new PrintWriter(socket.getOutputStream(), true);       //Opens a new printwriter for the socket
+            if(guess==correctValue){    //Game logic
                 sendWinPage(output);
             } else if(guess < correctValue){
                 sendHigherPage(output);
             } else{
                 sendLowerPage(output);
             }
-        }
     }
     private void sendStartPage() throws IOException {
         PrintWriter startup = new PrintWriter(socket.getOutputStream(), true);
@@ -125,7 +156,6 @@ class HTTPServer extends Thread {
         startup.println("<form action=\"\"> <label for=\"guess\"> guess number <input type=\"text\" id=\"guess\" name=\"guess\"> </label>  </form> ");
         startup.println();
         startup.close();
-        System.out.println("Game Started");
     }
     private void sendWinPage(PrintWriter output) {
         output.println("HTTP/1.1 200 OK");
@@ -136,12 +166,9 @@ class HTTPServer extends Thread {
         output.println("<form action=\"\"> <label for=\"guess\"> guess number <input type=\"text\" id=\"guess\" name=\"guess\"> </label>  </form> ");
         output.println();
         output.close();
-        Random rand = new Random();
-        correctValue = rand.nextInt(100);
+        correctValue = rand.nextInt(100);       //resetting the game
         doublettes = new int[100];
         guesses = 0;
-        System.out.println("Game Won");
-
     }
     private void sendHigherPage(PrintWriter output)  {
         output.println("HTTP/1.1 200 OK");
@@ -151,8 +178,6 @@ class HTTPServer extends Thread {
         output.println("<form action=\"\"> <label for=\"guess\"> guess number <input type=\"text\" id=\"guess\" name=\"guess\"></label>  </form> ");
         output.println();
         output.close();
-        System.out.println("Guess Higher");
-
     }
     private void sendLowerPage(PrintWriter output)  {
         output.println("HTTP/1.1 200 OK");
@@ -161,6 +186,6 @@ class HTTPServer extends Thread {
         output.println("<p> Nope, guess lower. You have made " + guesses + " guesses so far </p>");
         output.println("<form action=\"\"> <label for=\"guess\"> guess number <input type=\"text\" id=\"guess\" name=\"guess\"></label>  </form> ");
         output.println();
-        System.out.println("Guess Lower");
+        output.close();
     }
 }
